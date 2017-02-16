47a132606dd482c52a5304cdbed88f4b9916f5ee
Andrew Devlin and Philip Putnam <andrew@>
2017-02-16 09:49:06 -0800

    fulfill 1st spec

--- src/Scrabble.php
+++ src/Scrabble.php
@@ -3,6 +3,17 @@
     {
         function scrabbleScore($input)
         {
+            $one_points = array('a','e','i','o','u','l','n','r','s','t');
+            $input_letters = str_split($input);
+            $score = 0;
+            foreach ($input_letters as $input_letter) {
+                foreach ($one_points as $one_point) {
+                    if($input_letter == $one_point) {
+                        $score += 1;
+                    }
+                }
+            }
+            return $score;
 
         }
     }
